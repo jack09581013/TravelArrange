3ce25c16bfe7b2251dc74f947819293e057d10c9
Jack <z2589906@gmail.com>
2017-09-15 08:04:34 +0800

    增加輸出JSON格式的所有景點資料

--- Kaohsiung travel arrangements web/Controllers/HomeController.cs
+++ Kaohsiung travel arrangements web/Controllers/HomeController.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace Kaohsiung_travel_arrangements_web.Controllers
 {
+    
     public class HomeController : Controller
     {
+        private string root = "C:/Users/USER/Documents/Visual Studio 2015/Projects/TravelArrange/Kaohsiung travel arrangements web";
         // GET: Home
         public ActionResult SchedulePlanning()
-        {
-            MDTRA mdtra;
+        {            
             return View();
         }
 
         public ActionResult ScheduleResult()
         {
             return View();
+        }
+
+        public string AttractionsInfo()
+        {
+            List<string[]> data = new List<string[]>();
+            string[] source = System.IO.File.ReadAllLines(root + "/App_Data/attractions-all-information.csv");
+            foreach(string line in source)
+            {
+                data.Add(line.Split(new char[] {','}));
+            }
+
+            return JsonConvert.SerializeObject(data);
         }
     }
 }
