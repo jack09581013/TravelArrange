0c71d2e90536ecc33292b2968f5a397b8c927a71
Jack <z2589906@gmail.com>
2017-10-14 22:04:17 +0800

    防呆使用更美觀的介面

---
+++ Kaohsiung travel arrangements web/Scripts/message.js
@@ -0,0 +1,29 @@
+﻿//Message Utils
+var messageNo = 0;
+
+function addMessage() {
+    $('.messages').append('<div class="message ' + messageNo + '"><span></span></div>')
+    messageNo++;
+    return '.message.' + (messageNo - 1)
+}
+
+function correctShow(message) {
+    var selector = addMessage()
+    $(selector + ' span').text(message)
+    $(selector)
+        .css('background-color', '#21BA45')
+        .slideDown('slow')
+        .delay(2000)
+        .slideUp('slow', function () { $(this).remove() })
+
+}
+
+function errorShow(message) {
+    var selector = addMessage()
+    $(selector + ' span').text(message)
+    $(selector)
+        .css('background-color', '#DB2828')
+        .slideDown('slow')
+        .delay(2000)
+        .slideUp('slow', function () { $(this).remove() })
+}
